d9b6e43eceff827645f87e3fa40f06c721179d95
nian <tenianon@gmail.com>
2025-08-16 11:46:58 +0800

    chore: system store

--- src/stores/system.ts
+++ src/stores/system.ts
@@ -3,10 +3,6 @@ import { defineStore } from 'pinia'
 
 import packageJson from '@/../package.json'
 
-import { usePreferencesStore } from './preferences'
-import { useTabsStore } from './tabs'
-import { useUserStore } from './user'
-
 type SystemInfoOptions = {
   version: string
 }
@@ -16,25 +12,8 @@ const DEFAULT_SYSTEM_INFO_OPTIONS: SystemInfoOptions = {
 }
 
 export const useSystemStore = defineStore('systemStore', () => {
-  const rawSystemStore = localStorage.getItem('systemInfo')
 
   const systemInfo = useStorage<SystemInfoOptions>('systemInfo', DEFAULT_SYSTEM_INFO_OPTIONS)
-
-  const oldLocalStorage = localStorage.getItem('configure')
-  const storedVersion = rawSystemStore ? JSON.parse(rawSystemStore).version : null
-  const currentVersion = packageJson.version
-
-  if (oldLocalStorage || storedVersion) {
-    const shouldClear = !storedVersion || storedVersion !== currentVersion
-    if (shouldClear) {
-      useTabsStore().clearTabs()
-      usePreferencesStore().reset()
-      useUserStore().cleanup()
-      localStorage.clear()
-    }
-
-    systemInfo.value = { version: packageJson.version }
-  }
 
   return {
     systemInfo,
