ddad187dfc421a1907bfdd0ecb1f5b2e4c80d64c
nian <tenianon@gmail.com>
2026-03-14 02:35:29 +0800

    chore: window type

--- src/types/window.d.ts
+++ src/types/window.d.ts
@@ -1,5 +1,8 @@
 declare global {
   interface Window {
+    /*
+     * https://developer.mozilla.org/en-US/docs/Web/API/Navigation
+     */
     navigation?: {
       canGoBack: boolean
       canGoForward: boolean
